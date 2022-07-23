0d50d95056275f960710b882ce4e756f4ae54e26
Anna Raphaela Berto <raphaelaberto@gmail.com>
2022-07-23 10:38:39 -0300

    chore: adicionando scroll suave

--- script.js
+++ script.js
@@ -38,3 +38,23 @@ function initAccordion() {
   }
 }
 initAccordion();
+
+function initScrollSuave() {
+  const linksInternos = document.querySelectorAll('.js-menu a[href^="#"]');
+  function scrollToSection(e) {
+    e.preventDefault();
+    const href = e.currentTarget.getAttribute("href");
+    const section = document.querySelector(href);
+    const topo = section.offsetTop;
+    console.log(href);
+    window.scrollTo({
+      top: topo,
+      behavior: "smooth",
+    });
+  }
+
+  linksInternos.forEach((link) => {
+    link.addEventListener("click", scrollToSection);
+  });
+}
+initScrollSuave();
